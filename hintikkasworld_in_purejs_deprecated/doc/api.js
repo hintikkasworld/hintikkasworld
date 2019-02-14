3f7c5f487cb50d41bf4e24cf9cb0588c5a981386
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2019-02-14 14:55:53 +0100

    c'est le chantier

---
+++ hintikkasworld_in_purejs_deprecated/doc/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ActionModel",
+        "EpistemicModel",
+        "Graph",
+        "TrivialPostCondition",
+        "Valuation",
+        "createFormula",
+        "createLitteral",
+        "createWorldActionName",
+        "formulaPrettyPrint",
+        "product"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
---
+++ hintikkasworld_in_purejs_deprecated/js/symbolickripkemodel.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class SymbolicKripkeModel {
+	
+	constructor(spec) {
+		this.spec = spec;
+	}
+
+
+	getSuccessors(w, agent) {
+		let succs = [];
+		for(let i = 0; i < 10; i++) {
+			let val = 
+			succs.push();
+		}
+		return succs;
+	}
+	
+	
+
+
+}
