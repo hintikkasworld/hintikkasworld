95ebc464708499abd10df73ece0f5ef63cdca202
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2019-02-21 11:19:14 +0100

    BDD

--- src/app/services/bdd.service.ts
+++ src/app/services/bdd.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable, BehaviorSubject } from "rxjs";
 import { filter, map } from "rxjs/operators";
+import { isPlatformWorkerUi } from '@angular/common';
 
 //import * as Module from "./../../wasm/fibonacci.js";  <=== là on importe le JS d'Alexandre
 //import "!!file-loader?name=wasm/fibonacci.wasm!../../wasm/fibonacci.wasm";  // <==== là où on important le WASM d'Alexandre
@@ -8,8 +9,11 @@ import { filter, map } from "rxjs/operators";
 @Injectable({
   providedIn: 'root'
 })
+
+type BDDNode = number;
+
 export class BddService {
-  module: any;
+  bddModule: any;
 
   wasmReady = new BehaviorSubject<boolean>(false);
 
@@ -17,6 +21,8 @@ export class BddService {
   constructor() {
    // this.instantiateWasm("wasm/fibonacci.wasm"); <=============on load ici
    }
+
+
 
    private async instantiateWasm(url: string) {
     // fetch the wasm file
@@ -37,6 +43,19 @@ export class BddService {
 
     // instantiate the module
    // this.module = BDDModule(moduleArgs); <==== là on instancie
+
   }
+
+
+  isTrue(b: BDDNode): boolean {
+    return this.bddModule._is_true(b);
+  }
+
+  isFalse(b: BDDNode): boolean {
+    return this.bddModule._is_false(b);
+  }
+
+  
+
 
 }
