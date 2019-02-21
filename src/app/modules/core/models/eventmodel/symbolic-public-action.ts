8bcfaf5ddc050fed9913abf4906768b2d61288e4
Tristan Charrier <tristan.charrier@irisa.fr>
2019-02-21 14:39:48 +0100

    Commit des formules pour le public action symbolique

--- src/app/modules/core/models/eventmodel/symbolic-public-action.ts
+++ src/app/modules/core/models/eventmodel/symbolic-public-action.ts
@@ -2,6 +2,13 @@ import { EventModel } from './event-model';
 import { EpistemicModel } from '../epistemicmodel/epistemic-model';
 import { Formula } from '../formula/formula';
 
+/* 
+Nouvelles formules : 
+post(AP) = bigand_{a in AP} (p <-> Old(post(p)))
+chi = Forget(Old(pre and chi) and post(AP))
+Ra = Forget(Old(Ra) and post(AP) and post(AP'))
+*/
+
 export class SymbolicPublicAction implements EventModel {
     private precondition:Formula;
     private postcondition:Map<string,Formula>;
