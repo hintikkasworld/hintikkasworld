ccb8495e259cea0a1403ad11b77a0d0cd18552da
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2019-03-04 17:02:33 +0100

    spec symbolic epistemic model

--- src/app/modules/core/models/epistemicmodel/symbolic-epistemic-model.spec.ts
+++ src/app/modules/core/models/epistemicmodel/symbolic-epistemic-model.spec.ts
@@ -1,3 +1,4 @@
+import { WorldValuation } from './world-valuation';
 import { SymbolicEpistemicModel } from './symbolic-epistemic-model';
 import { BeloteTest } from './symbolic-epistemic-model';
 import { SymbolicRelation, Obs } from './symbolic-relation';
@@ -9,7 +10,7 @@ describe('SymbolicEpistemicModel', () => {
     let relationsSymboliques:Map<string, SymbolicRelation> = new Map(); 
     relationsSymboliques["a"] = new Obs(["var1"]);
     console.log(relationsSymboliques);
-    let sem = new SymbolicEpistemicModel(["var1", "var2"], [], new AtomicFormula("var1"), relationsSymboliques)
+    let sem = new SymbolicEpistemicModel(WorldValuation, ["var1", "var2"], [], new AtomicFormula("var1"), relationsSymboliques)
     expect(sem).toBeTruthy();
     expect(sem.getAgents() == ["a"])
   });
