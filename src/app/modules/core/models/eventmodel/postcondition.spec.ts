41f27453ea6892f3fc5dccedf99eb7c3122b3bce
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2019-04-11 18:23:20 +0200

    postconditino

---
+++ src/app/modules/core/models/eventmodel/postcondition.spec.ts
@@ -0,0 +1,32 @@
+import { WorldValuation } from './../epistemicmodel/world-valuation';
+import { ExplicitEpistemicModel } from './../epistemicmodel/explicit-epistemic-model';
+import { PropositionalAssignmentsPostcondition } from './propositional-assignments-postcondition';
+import { Valuation } from '../epistemicmodel/valuation';
+import { Postcondition } from './postcondition';
+
+describe('Postcondition', () => {
+  
+  it('cloneWorld: should clone', () => {
+    let world = new WorldValuation(new Valuation([]));
+    let world2 = Postcondition.cloneWorld(world);
+    expect(world != world2).toBeTruthy();
+  });
+
+  it('cloneWorld: should have different valuations', () => {
+    let world = new WorldValuation(new Valuation([]));
+    let world2 = Postcondition.cloneWorld(world);
+    expect(world.valuation != world2.valuation).toBeTruthy();
+  });
+
+  it('cloneWorld: should have different valuations and different propositions set', () => {
+    let world = new WorldValuation(new Valuation([]));
+    let world2 = Postcondition.cloneWorld(world);
+    expect(world.valuation.propositions != world2.valuation.propositions).toBeTruthy();
+  });
+
+  it('cloneWorld: should have the same value', () => {
+    let world = new WorldValuation(new Valuation([]));
+    let world2 = Postcondition.cloneWorld(world);
+    expect(world.modelCheck("p") == world2.modelCheck("p")).toBeTruthy();
+  });  
+});
