851d4bc9a07f02f64bd09f9b3409772cc123de7d
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2019-04-15 22:07:15 +0200

    cosmetics in the code

--- src/app/modules/core/models/epistemicmodel/symbolic-relation.ts
+++ src/app/modules/core/models/epistemicmodel/symbolic-relation.ts
@@ -4,25 +4,25 @@ import { BddService } from '../../../../services/bdd.service';
 import { BDD } from '../formula/bdd';
 
 export interface SymbolicRelation {
-    toFormula():Formula;
+    toFormula(): Formula;
     toBDD();
 }
 
-export class Obs implements SymbolicRelation{
+export class Obs implements SymbolicRelation {
 
-    protected variables:string[];
+    protected variables: string[];
 
-    constructor(atoms:string[]){
+    constructor(atoms: string[]) {
         this.variables = atoms;
     }
-    toFormula(){
-        let strFormula:string = "";
+    toFormula() {
+        let strFormula: string = "";
         this.variables.forEach(atom => {
             strFormula += "(" + atom + "<->" + SymbolicEpistemicModel.getPrimedVarName(atom) + ")";
         });
         return FormulaFactory.createFormula(strFormula);
     };
-    toBDD(){
+    toBDD() {
         return BDD.buildFromFormula(this.toFormula());
     };
 }
