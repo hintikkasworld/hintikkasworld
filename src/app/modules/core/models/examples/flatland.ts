7c0764186845e00e5a855eb828a255a69ff53551
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2019-04-17 12:12:41 +0200

    flatland

--- src/app/modules/core/models/examples/flatland.ts
+++ src/app/modules/core/models/examples/flatland.ts
@@ -1,8 +1,9 @@
+import { environment } from 'src/environments/environment';
 import { Formula } from './../formula/formula';
-import { Environment } from './../environment/environment';
 import { EpistemicModel } from './../epistemicmodel/epistemic-model';
 import { ExampleDescription } from '../environment/exampledescription';
 import { World } from '../epistemicmodel/world';
+import { Environment } from '../environment/environment';
 
 interface Point {
     x: number;
@@ -14,27 +15,50 @@ interface Point {
 class FlatlandWorld extends World {
     pos: { [agent: string]: Point };
     dir: { [agent: string]: number };
-
+    static readonly angleCone = 0.5;
 
     constructor(pos: { [agent: string]: Point }, dir: { [agent: string]: number }) {
         super();
         this.pos = pos;
         this.dir = dir;
 
-        for(let agent in pos) {
-            this.agentPos[agent] = {x: this.pos[agent].x, y: this.pos[agent].y, r: 8};
+        for (let agent in pos) {
+            this.agentPos[agent] = { x: this.pos[agent].x, y: this.pos[agent].y, r: 8 };
         }
     }
 
 
     draw(context: CanvasRenderingContext2D) {
+        function drawVisionCone(agent: string, p: Point, d: number) {
+            let l = 200;
+
+            context.beginPath();
+            context.strokeStyle = environment.agentColor[agent];
+            context.moveTo(p.x, p.y);
+            context.lineTo(p.x + l * Math.cos(d + FlatlandWorld.angleCone), p.y + l * Math.sin(d + FlatlandWorld.angleCone));
+            context.stroke();
+            context.moveTo(p.x, p.y);
+            context.lineTo(p.x + l * Math.cos(d - FlatlandWorld.angleCone), p.y + l * Math.sin(d - FlatlandWorld.angleCone));
+            context.stroke();
+        }
+
+
+        for (let agent in this.pos) {
+            drawVisionCone(agent, this.pos[agent], this.dir[agent]);
+        }
+
+
+
         this.drawAgents(context);
     }
 
 
     isSee(agent: string, agentb: string) {
-        return (this.pos[agentb].x - this.pos[agent].x) * Math.cos(this.dir[agent]) + 
-        (this.pos[agentb].y - this.pos[agent].y) * Math.sin(this.dir[agent]) >= 0;
+        let dist = Math.sqrt((this.pos[agentb].x - this.pos[agent].x) ^ 2 + (this.pos[agentb].y - this.pos[agent].y) ^ 2);
+        let scalarProduct = (this.pos[agentb].x - this.pos[agent].x) * Math.cos(this.dir[agent]) +
+            (this.pos[agentb].y - this.pos[agent].y) * Math.sin(this.dir[agent]) / dist;
+
+        return scalarProduct >= Math.cos(FlatlandWorld.angleCone);
     }
 
 
@@ -54,44 +78,63 @@ class FlatlandWorld extends World {
 
 
 class FlatlandEpistemicModel implements EpistemicModel {
-    pointedWorld;
+    static pointedWorld = new FlatlandWorld(
+        { a: { x: 10, y: 10 }, b: { x: 30, y: 10 }, c: { x: 10, y: 40 } },
+         { a: 0, b: 0, c: 0 });
 
-    constructor () {
-        this.pointedWorld = new FlatlandWorld({a: {x:10, y:10}, b: {x:30, y:10}, c: {x:10, y:40}}, {a: 0, b: 0, c: 0});
+    constructor() {
+
     }
 
 
     getPointedWorld(): World {
-        return this.pointedWorld;
+        return FlatlandEpistemicModel.pointedWorld;
     }
-    
-    
+
+
     getAgents(): string[] {
-         return ["a", "b", "c"];
+        return ["a", "b", "c"];
     }
 
 
     getSuccessors(w: FlatlandWorld, a: string) {
         function getSuccessor(w: FlatlandWorld, a: string) {
+            function testIfSuccessor(w: FlatlandWorld, u: FlatlandWorld) {
+                for (let agent of ["a", "b", "c"]) {
+                    if (!w.isSee(a, agent) && u.isSee(a, agent))
+                        return false;
+                }
+                return true;
+            }
+
             let newPos = {};
             let newDir = {};
 
-            for(let agent of ["a", "b", "c"]) {
-                if(w.isSee(a, agent) || a == agent) {
+            for (let agent of ["a", "b", "c"]) {
+                if (w.isSee(a, agent) || a == agent) {
                     newPos[agent] = w.pos[agent];
                     newDir[agent] = w.dir[agent];
                 }
                 else {
-                    newPos[agent] = {x: Math.random() * 128, y: Math.random() * 64};
+                    newPos[agent] = { x: Math.random() * 128, y: Math.random() * 64 };
                     newDir[agent] = Math.random() * 3.14 * 2;
                 }
             }
-            return new FlatlandWorld(newPos, newDir);
+
+            let potentialSuccessor = new FlatlandWorld(newPos, newDir);
+            if (testIfSuccessor(w, potentialSuccessor))
+                return potentialSuccessor;
+            else
+                return undefined;
+
         }
 
         let succs = [];
-        for(let i = 0; i < 10; i++)
-            succs.push(getSuccessor(w, a));
+        for (let i = 0; i < 50; i++) {
+            let u = getSuccessor(w, a);
+            if (u != undefined)
+                succs.push(u);
+        }
         return succs;
     }
 
@@ -117,7 +160,7 @@ export class Flatland implements ExampleDescription {
         return [];
     }
     getWorldExample(): World {
-        return new FlatlandWorld({a: {x:10, y:10}, b: {x:30, y:10}, c: {x:10, y:40}, d: {x:10, y:40}}, {a: 0, b: 0, c: 0, d: 0});
+        return FlatlandEpistemicModel.pointedWorld;
     }
     onRealWorldClick(env: Environment, point: any): void {
 
