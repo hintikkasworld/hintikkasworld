869fe17bcbec1c469266d12d9a518b67fcacc0b2
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2019-04-22 14:59:45 +0200

    sucessorset

--- src/app/modules/core/models/epistemicmodel/valuation.ts
+++ src/app/modules/core/models/epistemicmodel/valuation.ts
@@ -1,49 +1,40 @@
 export class Valuation {
-    propositions: { [id: string]: boolean } = {};
+  public readonly propositions: { [id: string]: boolean } = {};
 
-    constructor(truePropositions:  { [id: string]: boolean } | string[] ) {
-        if(truePropositions instanceof Array) {
-            this.propositions = {};
-            for(let proposition of truePropositions) 
-                this.propositions[proposition] = true;
-        }
-        else
-            this.propositions = truePropositions;
+  constructor(truePropositions: { [id: string]: boolean } | string[]) {
+    if (truePropositions instanceof Array) {
+      const props = {};
+      for (const proposition of truePropositions)
+        props[proposition] = true;
+      this.propositions = props;
     }
+    else
+      this.propositions = truePropositions;
+  }
 
-    
-    isPropositionTrue(p: string) {
-        return (this.propositions[p] == true);
-    }
+  isPropositionTrue(p: string) { return (this.propositions[p] == true); }
 
-    toAssignment(props: string[]): Map<string, boolean> {
-      /* check support */
-      for (const p of Object.keys(this.propositions)) {
-        if ( ! props.includes(p)) {
-          throw new Error("Valuation contains proposition " + p + " that is not in the support given");
-        }
+  toAssignment(props: string[]): Map<string, boolean> {
+    /* check support */
+    for (const p of Object.keys(this.propositions)) {
+      if (!props.includes(p)) {
+        throw new Error("Valuation contains proposition " + p + " that is not in the support given");
       }
-      const assignment = new Map();
-      for (const p of props) {
-        if (this.isPropositionTrue(p)) assignment.set(p, true);
-        else assignment.set(p, false);
-      }
-      return assignment;
     }
-
-
-
-    toString() {
-        let truePropositions = new Array();
-
-        for (let i in this.propositions)
-            if (this.propositions[i] == true)
-                truePropositions.push(i);
-
-        truePropositions.sort();
-
-        return truePropositions.join();
-
+    const assignment = new Map();
+    for (const p of props) {
+      if (this.isPropositionTrue(p)) assignment.set(p, true);
+      else assignment.set(p, false);
     }
+    return assignment;
+  }
 
+  toString() {
+    const truePropositions = new Array();
+    for (const proposition in this.propositions)
+      if (this.propositions[proposition] == true)
+        truePropositions.push(proposition);
+    truePropositions.sort();
+    return truePropositions.join();
+  }
 }
